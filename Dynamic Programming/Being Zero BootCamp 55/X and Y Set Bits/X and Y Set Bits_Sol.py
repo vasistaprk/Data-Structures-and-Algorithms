9c5d0d58a13159e4ab1ce027c01dac56b0493960
P.R.K.Vasista <vasista.ponnana@gmail.com>
2022-10-27 23:03:52 +0530

    Add files via upload

---
+++ Dynamic Programming/Being Zero BootCamp 55/X and Y Set Bits/X and Y Set Bits_Sol.py
@@ -0,0 +1,62 @@
+"""Given two numbers X and Y, find the number whose binary representation has its Xth and Yth bits 
+set to 1 and remaining bits set to 0.
+
+Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines. 
+Each subsequent line contains two integers: X - the index of the first set bit and Y - the index of 
+the second set bit, separated by a space.
+
+Constraints
+
+10 points
+1 <= T <= 100
+0 <= X, Y <= 30
+
+40 points
+1 <= T <= 105
+0 <= X, Y <= 105
+
+Output Format
+
+For each test case, print the number whose binary representation has its 
+Xth and Yth bits set to 1 and remaining bits set to zero, separated by a newline.
+Since this number can be very large, print the result % 1000000007.
+
+Sample Input 0
+
+3
+4 3
+5 0
+15 30
+Sample Output 0
+
+24
+33
+73774585
+Explanation 0
+
+Test Case 1
+The binary representation of the number that has bits at position 3 and 4 set is 11000 = 24
+
+Test Case 2
+The binary representation of the number that has the bit at position 5 and 0 set is 100001 = 33"""
+
+GT=[0]
+GT[0]=1
+M=1000000007
+for i in range(1,1000000):
+    s=(2*GT[i-1])%M
+    GT.append(s)
+
+import sys
+def prin(a):
+    sys.stdout.write(str(a)+"\n")
+
+T=int(input())
+for i in range(T):
+    x,y=[int(i) for i in (input().split())]
+    if(x!=y):
+        prin(((GT[x])%M+(GT[y])%M)%M)
+    else:
+        prin((GT[x])%M)

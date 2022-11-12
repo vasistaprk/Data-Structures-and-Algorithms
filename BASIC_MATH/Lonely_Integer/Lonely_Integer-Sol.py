5ed82a1ddde0889ca50ff44df12d090346065530
vasistaprk <vasista.ponnana@gmail.com>
2022-11-12 16:32:00 +0530

    Lonely_Integer

---
+++ BASIC_MATH/Lonely_Integer/Lonely_Integer-Sol.py
@@ -0,0 +1,19 @@
+
+
+def lonelyinteger(a):
+
+    for i in range(0,len(a)):
+        x=a[i]
+        count=0
+        for j in range(0,len(a)):
+            if(x^a[j]==0 ):
+                count+=1
+        if(count==1):
+            return(x)
+            
+
+n = int(input().strip())
+a = list(map(int, input().rstrip().split()))
+result = lonelyinteger(a)
+print(result)
+  

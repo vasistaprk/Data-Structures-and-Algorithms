d8b87edab44f68d1d9f48f033dd20ca012732733
vasistaprk <vasista.ponnana@gmail.com>
2022-11-15 04:45:16 +0530

    Sparse Matrix

--- BASIC_MATH/Sparse_Matrix/Sparse_Matrix_Sol.java
+++ BASIC_MATH/Sparse_Matrix/Sparse_Matrix_Sol.java
@@ -4,24 +4,25 @@ import java.util.*;
 public class Sparse_Matrix_Sol {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        Scanner io =new Scanner(System.in);
-        int n= io.nextInt();
-        int m= io.nextInt();
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                int a=io.nextInt();
-                if(a==0){
-                    count+=1;
+        try (/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner io = new Scanner(System.in)) {
+            int n= io.nextInt();
+            int m= io.nextInt();
+            int count=0;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    int a=io.nextInt();
+                    if(a==0){
+                        count+=1;
+                    }
                 }
             }
-        }
-        if(count>((n*m)/2)){
-            System.out.println("Yes");
-        }
-        else{
-            System.out.println("No");
+            if(count>((n*m)/2)){
+                System.out.println("Yes");
+            }
+            else{
+                System.out.println("No");
+            }
         }
     }
 }

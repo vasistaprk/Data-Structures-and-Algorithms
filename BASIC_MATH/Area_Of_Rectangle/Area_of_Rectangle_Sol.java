316e07f19c7aec8aebb26ad80a1b56d05161b251
vasistaprk <vasista.ponnana@gmail.com>
2022-11-15 05:13:57 +0530

    Area of Rectangle

---
+++ BASIC_MATH/Area_Of_Rectangle/Area_of_Rectangle_Sol.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Area_of_Rectangle_Sol {
+    public static void main(String[] args) {
+        try (Scanner io = new Scanner(System.in)) {
+            long l=io.nextLong();
+            long b=io.nextLong();
+            System.out.println(l*b);
+        }
+    }
+    
+}

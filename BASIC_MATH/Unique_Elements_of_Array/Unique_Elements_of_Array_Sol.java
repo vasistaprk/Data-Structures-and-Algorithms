8a30153ad593eb5b1d94d70f3e6223d45082352b
vasistaprk <vasista.ponnana@gmail.com>
2022-11-15 04:44:05 +0530

    Unique Element of Array

---
+++ BASIC_MATH/Unique_Elements_of_Array/Unique_Elements_of_Array_Sol.java
@@ -0,0 +1,33 @@
+
+import java.util.*;
+
+public class Unique_Elements_of_Array_Sol {
+
+    public static void main(String[] args) {
+        try (/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner io = new Scanner(System.in)) {
+            int x= io.nextInt();
+            int [] ar = new int[x];
+            for(int i=0;i<x;i++){
+                ar[i]=io.nextInt();   
+            }
+            for(int i=0;i<x;i++){
+                int count=1;
+                for(int j=i+1;j<x;j++){
+                    if(ar[j]==ar[i] && ar[j]!=-1){
+                        count+=1;
+                        ar[j]=-1;
+                    }
+                }
+                if(count==1 && ar[i]!=-1){
+                    System.out.print(ar[i]+" ");
+                }
+            }
+        }
+    }
+}
+
+
+
+//Time Complexity: O(N^2)
+//Space CVomplexity: O(1)

1de05c2098e278076c73ea1c669cadc6e3410040
vasistaprk <vasista.ponnana@gmail.com>
2022-11-30 20:03:20 +0530

    SUM's of First N Natural Numbers

---
+++ BASIC_MATH/Sums_of_First_N.py
@@ -0,0 +1,20 @@
+"""
+Sum Of First N natural Numbers:
+"""
+n=int(input())
+ans = (n*(n+1))/2
+print(int(ans))
+
+"""
+Sum Of Sqaures of First N natural Numbers:
+"""
+n=int(input())
+ans = (n*(n+1)*(2*n+1))/6
+print(int(ans))
+
+"""
+Sum Of Cubes of First N natural Numbers:
+"""
+n=int(input())
+ans = (n*n*(n+1)*(n+1))/4
+print(int(ans))
